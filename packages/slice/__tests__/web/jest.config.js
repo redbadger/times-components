9b3ccfc11c7b5acda0c2f1e5a6ee47344d9b13b8
Craig Bilner <craig.bilner@news.co.uk>
2018-06-14 18:17:53 +0100

    test: add

--- packages/slice/__tests__/web/jest.config.js
+++ packages/slice/__tests__/web/jest.config.js
@@ -1,5 +1,7 @@
 const jestConfigurator = require("@times-components/jest-configurator").default;
+const path = require("path");
 
 module.exports = jestConfigurator("web", __dirname, {
-  coverageIgnoreGlobs: ["index.js", "styles.js"]
+  coverageIgnoreGlobs: ["index.js", "styles.js"],
+  setupTestFrameworkScriptFile: path.join(__dirname, "./serializers")
 });
