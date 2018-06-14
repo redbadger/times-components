efd281686ea81eb3e7543cc04e064d0a52828ba7
Craig Bilner <craig.bilner@news.co.uk>
2018-06-14 13:27:00 +0100

    test: add

---
+++ packages/article-list/__tests__/web/serializers.js
@@ -0,0 +1,13 @@
+const {
+  compose,
+  flattenStyleTransform,
+  minimalWebTransform,
+  rnwTransform,
+  rnwPrinter
+} = require("@times-components/jest-serializer");
+const { createSerializer } = require("enzyme-to-json");
+
+expect.addSnapshotSerializer(createSerializer());
+expect.addSnapshotSerializer(
+  compose(rnwPrinter, flattenStyleTransform, minimalWebTransform, rnwTransform())
+);
