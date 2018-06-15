6705792b955e56aa1236d0eaee3dd8db6d6713f3
Craig Bilner <craig.bilner@news.co.uk>
2018-06-15 12:14:41 +0100

    test: add

--- packages/tracking/__tests__/web/jest.config.js
+++ packages/tracking/__tests__/web/jest.config.js
@@ -1,5 +1,7 @@
 const jestConfigurator = require("@times-components/jest-configurator").default;
+const path = require("path");
 
 module.exports = jestConfigurator("web", __dirname, {
-  coverageIgnoreGlobs: ["storybook-components"]
+  coverageIgnoreGlobs: ["storybook-components"],
+  setupTestFrameworkScriptFile: path.join(__dirname, "./serializers")
 });
